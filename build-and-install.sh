b6d202e63e731fc442bf30b00c378c79ba9c1d8a
morganhawkins <66576505+morganhawkins@users.noreply.github.com>
2023-06-01 13:33:56 -0400

    created build and isntall bash file

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,6 +1,47 @@
 #!/usr/bin/env bash
 
+#setting directory to library folder
+echo
+echo
 
+cd /Users/morganhawkins/Desktop/first_python_lib
+
+echo set working directory to library folder
+echo
+echo
+
+
+
+echo
+echo
+
+python setup.py pytests
+
+echo tests passed!
+echo
+echo
+
+
+
+echo
+echo
+
+python setup.py bdist_wheel
+
+echo built wheel file
+echo
+echo
+
+
+echo
+echo
+
+pip3 uninstall /Users/morganhawkins/Desktop/first_python_lib/dist/nnlearn-1.0-py3-none-any.whl
+pip3 install /Users/morganhawkins/Desktop/first_python_lib/dist/nnlearn-1.0-py3-none-any.whl
+
+echo installed library to python
+echo
+echo
 
 
 
